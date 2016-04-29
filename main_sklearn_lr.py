55995ad346be519c312603b44d613bb5f25ef11f
Sasha <sasha.pazyuk@gmail.com>
2016-04-29 20:53:35 +0300

    import statements improvement

--- main_sklearn_lr.py
+++ main_sklearn_lr.py
@@ -1,9 +1,9 @@
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.linear_model import LogisticRegression
-from utils import *
+import utils as utl
 
 
-cuisine_list, ingredients_list, x, y = load_train('number')
+cuisine_list, ingredients_list, x, y = utl.load_train('number')
 classifier = OneVsRestClassifier(LogisticRegression(C=1e6)).fit(x, y)
 p = classifier.predict(x)
 
@@ -15,8 +15,8 @@ accuracy = (1.0 * precision) / len(y)
 
 print('Training Set Accuracy:', accuracy)
 
-t, ids = load_test(ingredients_list)
+t, ids = utl.load_test(ingredients_list)
 p = classifier.predict(t)
-save_result('sk_lr', cuisine_list, p, ids, 'number')
+utl.save_result('sk_lr', cuisine_list, p, ids, 'number')
 
 
